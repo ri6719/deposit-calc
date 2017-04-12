21d62e27e7787b93d17f297280488ae277d6ae30
ri6719 <yasenkoolesya@yandex.ru>
2017-04-12 09:12:07 +0700

    new var2

---
+++ var.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void solution(int sum, int date)
+{
+   if (dat<=365 && sum>=10000 && sum<=100000){
+        if (date>0 && date<=30){
+       sum = sum - sum * 0.10;
+        }
+        if (date>30 && date<=120){
+        sum = sum + sum * 0.02;
+        }
+        if (date>120 && date<=240){
+        sum = sum+sum*0.06;
+        }
+        if (date>240 && date<=365){
+        sum = sum+sum*0.12;
+        }
+    }
+    else{
+         printf("Error!");
+         }
+
+    if (date<=365 && sum>=100000){
+        if (date>0 && date<=30){
+        sum = sum-sum*0.10;
+        }
+        if (date>30 && date<=120){
+        sum = sum+sum*0.03;
+        }
+        if (date>120 && date<=240){
+        sum = sum+sum*0.08;
+        }
+        if (date>240 && date<=365){
+        sum = sum+sum*0.15;
+        }
+      }
+    else{
+         printf("Error");
+        }
+    printf("Sum: %d\n", sum);
+}
+
+int main()
+{
+    int Sum,Date;
+
+    printf("Enter sum and date:");
+     scanf("%d %d", Sum, Date);
+    
+    return 0;
